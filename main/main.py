bfc95373c62564b09ce0017b3fa06256331746fd
Elias Fandi <elias@fandi.me>
2020-11-14 18:59:59 +0000

    Add websockets and test html file

--- main/main.py
+++ main/main.py
@@ -1,6 +1,43 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_python38_app]
+from flask import Flask, render_template
+from flask_socketio import SocketIO, join_room, emit, send
+import json
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+socketio = SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
+
+@app.route('/')
 def main():
-    print("game ended")
+   
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    main()
+@socketio.on('create')
+def on_create(data):
+    k = 0
+    print(str(data))
+    while k < 23:
+        emit('connection', {'status': 'worked'})
+        k = k+ 1
 
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    socketio.run(app, debug=True)
+# [END gae_python38_app]
