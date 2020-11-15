536c7c225e7d382d4e4b018d508c08b708d665c4
Elias Fandi <elias@fandi.me>
2020-11-15 11:57:33 +0000

    Player instantiation method

--- main/backend/game_objects/Game.py
+++ main/backend/game_objects/Game.py
@@ -1,12 +1,19 @@
 import time
-
+from Player import *
 class Game(object):
     def __init__(self, Player1):
         self.Player1 = Player1
         self.Player2 = None
+        self.playerOneObj = None
+        self.PlayerTwoObj = None
 
     def addPlayer(self, Player):
         self.Player2 = Player
+    
+    def instantiatePlayers(self):
+        self.playerOneObj = Player(self.Player1, 100, 100, 1, 'Alive', 0, 0)
+        self.PlayerTwoObj = Player(self.Player2, 100, 100, 1, 'Alive', 0, 0)
+
 
 
 
